4cac880b5523e9ba1671099769635211e45e3394
djib2011 <thanostagaris@gmail.com>
2021-01-05 11:29:34 +0200

    Initial commit

---
+++ evaluation/__init__.py
@@ -0,0 +1,7 @@
+"""
+Evaluation scheme is based on TimeGAN:
+https://papers.nips.cc/paper/2019/file/c9efe5f26cd17ba6216bbe2a7d26d490-Paper.pdf
+https://github.com/jsyoon0823/TimeGAN/tree/master/metrics
+"""
+
+import discriminative, predictive, visualization
