1c68b07de93ec3b3d0c5a53c487ebaaeea7d6e17
djib2011 <thanostagaris@gmail.com>
2021-01-28 15:45:34 +0200

    Update vanilla training with undersampled dataset

--- training/vanilla.py
+++ training/vanilla.py
@@ -8,10 +8,11 @@ sys.path.append(os.getcwd())
 import datasets
 import models
 
-dataset_name = '14k'
+dataset_name = '4k'
 
 name = 'vanilla_lstm_large'
 batch_size = 512
+epochs = 100
 result_dir = 'results/{}_{}/'.format(name, dataset_name)
 report_dir = 'reports/{}_{}/'.format(name, dataset_name)
 
@@ -24,9 +25,14 @@ gan = model_gen(hparams)
 if dataset_name == '235k':
     train_path = 'data/yearly_24_train.h5'
     test_path = 'data/yearly_24_test.h5'
-else:
+elif dataset_name == '14k':
     train_path = 'data/yearly_24_nw_train.h5'
     test_path = 'data/yearly_24_nw_test.h5'
+elif dataset_name == '4k':
+    train_path = 'data/yearly_24_undersampled.h5'
+    test_path = 'data/yearly_24_nw_test.h5'
+else:
+    raise ValueError('Invalid dataset_name.')
 
 train_gen = datasets.gan_generator(train_path, batch_size=1024, shuffle=True)
 valid_gen = datasets.gan_generator(test_path, batch_size=1024, shuffle=True)
@@ -35,7 +41,8 @@ g_losses, d_losses = gan.train(train_gen, valid_gen,
                                train_steps=len(train_gen) // batch_size + 1,
                                valid_steps=len(valid_gen) // batch_size + 1,
                                result_dir=result_dir,
-                               save_weights=True)
+                               save_weights=True,
+                               epochs=epochs)
 
 report_dir = Path(report_dir)
 
